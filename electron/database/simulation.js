48f7f2a60e0d7e24c1730506ddd394823a866ceb
Carson Gabler <102882837+cbgabler@users.noreply.github.com>
2025-11-06 17:41:01 -0800

    feat: Session skeleton

--- electron/database/simulation.js
+++ electron/database/simulation.js
@@ -4,11 +4,22 @@ let sessions = {}
 
 // Generate the current sessions
 // vars: scenarioId (current selected scenario), userId (current user)
-export function start(scenarioId, userId) {
+export function endSession(scenarioId, userId) {
   const db = getDb()
   const currScenario = db.prepare('SELECT * FROM scenarios WHERE id=?').get(scenarioId)
 
   const sessionId = db.prepare(`
-    INSERT INTO sessions ()
-  `)
+    INSERT INTO sessions (scenarioId, userId, started) VALUES (?, ?, ?)
+  `).run(scenarioId, userId, new Date())
+
+  // Current state of the scenario (default)
+  const currState = JSON.parse(scenario) // TODO need to make something that defines the current scenario, like a state definition table
+
+  sessions[sessionId].interval = setInterval(() => tick(sessionId), 1000);
+
+  return { sessionId, vitals: initialState };
+}
+
+export function endSession(sessionId) {
+  get
 }
