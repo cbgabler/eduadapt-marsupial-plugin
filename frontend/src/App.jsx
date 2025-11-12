0a2d2278acc2e06b23ffaac0ba3a6874e1cab7d6
Carson Gabler <102882837+cbgabler@users.noreply.github.com>
2025-11-11 20:19:04 -0800

    feat: WIP navbar for main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-import RegisterForm from './RegisterForm';
+import RegisterForm from './RegisterForm.jsx';
+import Modules from './Modules.jsx'
 import './App.css';
+
+import Dropdown from 'react-bootstrap/Dropdown';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
   return (
-    <div className="app">
-      <RegisterForm />
-    </div>
+    <Nav
+      activeKey="/home"
+      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    >
+      <Nav.Item as='li'>
+        <Nav.Link href="/home">Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as='li'>
+        <Nav.Link href="/RegisterForm">Register</Nav.Link>
+      </Nav.Item>
+      <Nav.Dropdown title="SignIn" id="signin-dropdown">
+        <Nav.Link href="/SignIn">SignIn</Nav.Link>
+        <Nav.Link href="/SignUp">SignUp</Nav.Link>
+      </Nav.Dropdown>
+    </Nav>
   );
 }
 
